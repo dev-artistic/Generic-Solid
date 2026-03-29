df157286888d675a2669158c004ad7fa22ca2e9d
dev-artistic <dev.artistic.01@gmail.com>
2026-03-29 23:58:20 +0530

    ROute protection-1

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,23 +5,30 @@ import { publicRoutes } from './publicRoutes'
 import { privateRoutes } from './protectedRoutes'
 import type { JSXElement } from 'solid-js'
 import { NavigationComponent } from "../components/navbar/navbar";
+import { createSignal } from 'solid-js'
 
 
+const [user, setUser] = createSignal(null)
 export const rootRoute = createRootRoute({
     component: () => (
         <>
             <NavigationComponent />
             <Outlet/>
         </>
-    )
+    ),
+    context: {
+        user
+    }
 })
 const routeTree = rootRoute.addChildren([...publicRoutes, ...privateRoutes])
 export const router = createRouter({ routeTree })
+
 
 const navigate = useNavigate()
 export const RouterNavigation = ({href = "/"}:{href?: string}) => {
     navigate({to:href})
 }
+
 
 export const RouterLink = ({ href = "/", children }: { href?: string, children: JSXElement }) => {
     return (
